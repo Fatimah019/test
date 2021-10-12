0bbd16db43138c6fed4d8bf95651bc43e797cb79
Davies Fatimah Omowonuola <daviesfatimah97@gmail.com>
2021-10-12 05:17:48 +0100

    nav bar

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaBars, AiOutlineClose } from "react-icons/all";
 
 const Header = () => {
+  const [showNavmenu, setShowNavmenu] = useState(false);
+  const [showopenicon, setShowopenicon] = useState(true);
+
+  const handleNavBar = () => {
+    setShowNavmenu(true);
+    setShowopenicon(false);
+  };
+
+  const closeNavMenu = () => {
+    setShowNavmenu(false);
+    setShowopenicon(true);
+  };
+
+  const style = showNavmenu ? { display: "block" } : {};
+
   return (
     <header>
       <div className="header_left">
@@ -13,13 +28,20 @@ const Header = () => {
           <FaSearch />
         </label>
       </div>
-      <div className="header_right">
+      <div className="header_right_desktop">
         <a href="/" className="quote_link">
           Request Quote
         </a>
         <a href="/" className="shipment_link">
           Book Shipment
         </a>
+      </div>
+      <div className="header_right_mobile" style={style}>
+        {showopenicon ? (
+          <FaBars onClick={handleNavBar} style={{ fontSize: "2rem" }} />
+        ) : (
+          <AiOutlineClose onClick={closeNavMenu} style={{ fontSize: "2rem" }} />
+        )}
       </div>
     </header>
   );
